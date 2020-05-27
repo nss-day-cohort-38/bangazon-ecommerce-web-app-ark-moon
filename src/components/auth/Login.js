b24eaf7547a5aec3af49ea6bc9c2af41482668be
Matt Crook <matt.crook11@gmail.com>
2020-05-27 12:20:20 -0500

    fix: Register and Login fetch calls edited. Token session storage moved to the API module where it sets the token to be used.

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,54 +1,62 @@
-import React, {useState, useEffect} from "react"
-import AuthManager from "../../modules/AuthManager.js"
+import React, { useState, useEffect } from "react";
+import AuthManager from "../../modules/AuthManager.js";
 
-const Login = ({routerProps}) => {
-
-  const [loggedIn, setIsLoggedIn] = useState(false)
-  const [credentials, setCredentials] = useState({username: "", password: ""})
+const Login = ({ routerProps }) => {
+  const [loggedIn, setIsLoggedIn] = useState(false);
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
 
   const handleFieldChange = (evt) => {
-    const stateToChange = {...credentials}
-    stateToChange[evt.target.id] = evt.target.value
-    setCredentials(stateToChange)
-  }
+    const stateToChange = { ...credentials };
+    stateToChange[evt.target.id] = evt.target.value;
+    setCredentials(stateToChange);
+  };
 
   const handleLogin = (e) => {
-      e.preventDefault()
-      const customerCreds = {
-          "username": credentials.username, 
-          "password": credentials.password
-      }
-      return AuthManager.loginUser(customerCreds)
-      .then(parsedResponse => {
-          if("valid" in parsedResponse && parsedResponse.valid && "token" in parsedResponse) {
-              sessionStorage.setItem("token", parsedResponse.token)
-              setIsLoggedIn(true)
-          }
-      })
-      .then(() => routerProps.history.push("/"))
-  }
-
- 
+    e.preventDefault();
+    const customerCreds = {
+      username: credentials.username,
+      password: credentials.password,
+    };
+    AuthManager.loginUser(customerCreds).then(() => {
+      setIsLoggedIn(true);
+      routerProps.history.push("/");
+    });
+  };
 
   return (
-   <>
-   <form onSubmit={handleLogin}>
-            <h1>Login</h1>
-            <fieldset>
-                <label htmlFor="username">Username</label>
-                <input onChange={handleFieldChange} type="text" id="username" placeholder="Username" value={credentials.username}/>
-            </fieldset>
-            <fieldset>
-                <label htmlFor="password">Password</label>
-                <input onChange={handleFieldChange} type="password" id="password" placeholder="Password" value={credentials.password}/>
-            </fieldset>
-            <fieldset>
-            <button type="Submit">Login</button>
-            </fieldset>
-        </form>
-    <a href="http://localhost:3000/register">Register</a>
-   </>
+    <>
+      <form onSubmit={handleLogin}>
+        <h1>Login</h1>
+        <fieldset>
+          <label htmlFor="username">Username</label>
+          <input
+            onChange={handleFieldChange}
+            type="text"
+            id="username"
+            placeholder="Username"
+            value={credentials.username}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="password">Password</label>
+          <input
+            onChange={handleFieldChange}
+            type="password"
+            id="password"
+            placeholder="Password"
+            value={credentials.password}
+          />
+        </fieldset>
+        <fieldset>
+          <button type="Submit">Login</button>
+        </fieldset>
+      </form>
+      <a href="http://localhost:3000/register">Register</a>
+    </>
   );
 };
 
-export default Login
+export default Login;
--- src/modules/AuthManager.js
+++ src/modules/AuthManager.js
@@ -1,26 +1,34 @@
-const baseUrl = "http://127.0.0.1:8000/"
+const baseUrl = "http://127.0.0.1:8000/";
 
 const AuthManager = {
-    loginUser(user) {
-        return fetch(`${baseUrl}login/`, {
-            method: "POST", 
-            headers: {
-                "Content-Type": "application/json", 
-                "Accept": "application/json"
-            }, 
-            body: JSON.stringify(user)
-        }).then(resp => resp.json())
-    }, 
-    registerUser(user) {
-        return fetch(`${baseUrl}register/`, {
-            method: "POST", 
-            headers: {
-                "Content-Type": "application/json", 
-                "Accept": "application/json"
-            }, 
-            body: JSON.stringify(user)
-        }).then(resp => resp.json())
-    }
-}
+  loginUser(user) {
+    return fetch(`${baseUrl}login/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(user),
+    }).then((resp) => {
+      resp.json().then((resp) => {
+        sessionStorage.setItem("token", resp.token);
+      });
+    });
+  },
+  registerUser(user) {
+    return fetch(`${baseUrl}register/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(user),
+    }).then((resp) =>
+      resp.json().then((token) => {
+        sessionStorage.setItem("token", token.token);
+      })
+    );
+  },
+};
 
-export default AuthManager
+export default AuthManager;
