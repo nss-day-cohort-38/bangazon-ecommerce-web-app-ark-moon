b94e47da88def2ee9c56f2aac2793ecd247064fc
Jeremy Mattingly <jamattingly@live.com>
2020-05-27 15:01:14 -0500

    cards are now linkable to details

--- src/components/buy/ProductCard.js
+++ src/components/buy/ProductCard.js
@@ -2,11 +2,18 @@ import React from 'react'
 import { Card, Avatar } from 'antd';
 import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 
+
 const { Meta } = Card;
 
 const ProductCard = ({productObj}) => {
+
+    function routePath(){
+        window.location = `/buy/${productObj.id}`
+    }
+
   return(
   <Card
+    onClick={routePath}
     className='productCard'
     cover={
       <img
