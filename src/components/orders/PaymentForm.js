8a4d68a7ac2e4cb1f903f9cca614b80de1c6f41f
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-06-05 15:12:18 -0500

    working on getting product quantity to lower on order completion

--- src/components/orders/PaymentForm.js
+++ src/components/orders/PaymentForm.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import orderManager from '../../modules/orderManager';
 import paymentTypeManager from '../../modules/paymentTypeManager';
+import productManager from '../../modules/productManager';
+import orderProductManager from '../../modules/orderProductManager';
 
 const PaymentForm = (routerProps) => {
     // used to store all the user's orders
@@ -9,8 +11,35 @@ const PaymentForm = (routerProps) => {
     const [openOrder, setOpenOrder] = useState([]);
     //used to store all the user's payment types
     const [paymentTypes, setPaymentTypes] = useState([]);
+    // this stores all of the products available on Bangazon
+    const [products, setProducts] = useState([]);
+    // this stores the products that are connected with the active order
+    const [shoppingCart, setShoppingCart] = useState([]);
+    
 
     const props = routerProps.routerProps;
+
+    const getProducts = () => {
+        productManager.getProducts().then(products => {
+            setProducts(products)
+        })
+    }
+
+    const getShoppingCartProducts = () => {
+        // get all of the users orders
+        orderManager.getUserOrders().then(orders => {
+            setOrders(orders)
+            // filter out just the single open order
+            const openOrder = orders.filter(order => order.payment_type === null)
+            setOpenOrder(openOrder)
+            // if there is an open order, get the products associated with that order
+            if (openOrder.length !== 0) {
+                orderProductManager.getOrderProductsByOrder(openOrder[0].id).then(products => {
+                    setShoppingCart(products)
+                })
+            } 
+        })
+    }
 
     const handleFieldChange = evt => {
         const stateToChange = { ...openOrder[0] };
@@ -40,6 +69,8 @@ const PaymentForm = (routerProps) => {
                 .then(() => {
                     props.history.push("/currentorder")
                 })
+            
+            
         }
     }
 
@@ -61,11 +92,14 @@ const PaymentForm = (routerProps) => {
     useEffect(() => {
         getOpenOrder();
         getPaymentTypes();
+        getProducts();
+        getShoppingCartProducts();
     }, []);
 
     return (
         <>
             <h1>Select Payment Type:</h1>
+            <button type="button" onClick={() => props.history.push("newpayment")}>Add a new payment account</button>
             <fieldset>
                 <select
                     className="form-control"
